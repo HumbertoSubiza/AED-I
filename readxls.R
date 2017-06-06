ebbd9aa9ff049f5bb308d0d35ffe70ca03060c84
Walter Humberto Subiza Piña <sirio@ig.com.br>
2017-06-06 12:36:15 -0300

    Add files via upload

---
+++ readxls.R
@@ -0,0 +1,12 @@
+library(readxl)
+readxl_example()
+
+clippy <- readxl_example("clippy.xls")
+
+xlsx_example <- readxl_example("datasets.xlsx")
+
+x <- read_excel(xlsx_example)
+
+(path <- getwd())
+excel_sheets("C:/Users/humberto.pina/Documents/cursos IBGE/Cursos2017/Oficina AED com R-I/02 R/01 Importação/02 planilhas/")
+dir()
